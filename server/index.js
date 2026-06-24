ba1ca9e81fd072d62801103dc36930d066eea4a7
RITWIZ <ritwizhota@gmail.com>
2026-06-25 04:27:47 +0530

    feat: production env config

--- server/index.js
+++ server/index.js
@@ -7,7 +7,10 @@ const app = express()
 const PORT = process.env.PORT || 3000
 
 app.use(cors({
-  origin: 'http://localhost:5173',
+  origin: [
+    'http://localhost:5173',
+    /\.netlify\.app$/,
+  ],
   methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization']
 }))
